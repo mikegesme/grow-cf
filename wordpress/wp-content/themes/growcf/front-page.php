3d2b77d0909859e3eb34b8e456a7d87765616f86
U-CITY\mgesme <mikegesme@users.noreply.github.com>
2015-03-24 15:59:16 -0500

    Removed extra success stories.

--- wordpress/wp-content/themes/growcf/front-page.php
+++ wordpress/wp-content/themes/growcf/front-page.php
@@ -30,30 +30,6 @@
 					<p>Joe and Julie Disch are proud owners of Hi-Quality Bakery in Cannon Falls. As owners since 1992, they have provided the very best in bakery products. In 2006, the bakery celebrated it's 60th year in business and look forward to many more years to come.</p>
 				</div>
 			</div>
-		</div><div class="testimonial testimonial-left">
-			<div class="media">
-				<div class="media-left">
-					<figure>
-						<img class="media-object" src="<?php bloginfo('template_url'); ?>/images/maloneys.jpg">
-						<figcaption>John & Maureen Maloney, Owners<br>Cannon River Winery</figcaption>
-					</figure>
-				</div>
-				<div class="media-body">
-					<p>THIRD  hester, historic downtown Cannon Falls is the perfect destination trip for those wishing to enjoy the beauty and amenities the area has to offer.  The support offered by the city, county, and surrounding community makes Cannon Falls a great place to establish a business.”</p>
-				</div>
-			</div>
-		</div><div class="testimonial testimonial-right">
-			<div class="media">
-				<div class="media-left">
-					<figure>
-						<img class="media-object" src="<?php bloginfo('template_url'); ?>/images/maloneys.jpg">
-						<figcaption>John & Maureen Maloney, Owners<br>Cannon River Winery</figcaption>
-					</figure>
-				</div>
-				<div class="media-body">
-					<p>FOURTH  hester, historic downtown Cannon Falls is the perfect destination trip for those wishing to enjoy the beauty and amenities the area has to offer.  The support offered by the city, county, and surrounding community makes Cannon Falls a great place to establish a business.”</p>
-				</div>
-			</div>
 		</div>
 	</div>
 </div>
